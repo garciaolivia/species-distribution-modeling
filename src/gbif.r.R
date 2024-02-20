14cff5337f3715340fdb8cf3937fdb8d1494c86d
Olivia Garcia <garciaolivia@lclark.edu>
2024-02-20 20:03:08 +0000

    data cleaning

--- src/gbif.r.R
+++ src/gbif.r.R
@@ -62,6 +62,24 @@ fData<-fData %>%
 fData<- fData %>%
   filter(!basisOfRecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN"))
 
+fData <- fData %>%
+  cc_sea(lon= "decimalLongitude", lat="decimalLatitude")
+
+#remove duplication
+fData <- fData %>%
+  distinct(decimalLongitude, decimalLatitude, speciesKey, datasetKey, .keep_all=TRUE)
+
+#one fell swoop:
+cleanData <- d %>%
+  filter(!is.na(decimalLatitude), !is.na(decimalLongitude)) %>%
+  filter(countryCode %in% c("US", "CA", "MX")) %>%
+  filter(!basisOfRecord %in% c("FOSSIL_SPECIMEN", "LIVING_SPECIMEN")) %>%
+  cc_sea(lon= "decimalLongitude", lat="decimalLatitude") %>%
+  distinct(decimalLongitude, decimalLatitude, speciesKey, datasetKey, .keep_all=TRUE)
+  
+  
+  
+  
 
 
 
